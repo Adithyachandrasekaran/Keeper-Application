256b12921ca74ae0141dba9fbe613687f14c1eed
Adithya <madavadithya@gmail.com>
2021-05-29 12:04:24 +0530

    Initial commit

---
+++ build/precache-manifest.ba2fe78a8fc5e7b83a3471b49489d5e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6488d942a761702e17eebe6d75803075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbbed2bf7928a43be96",
+    "url": "/static/js/2.fabae278.chunk.js"
+  },
+  {
+    "revision": "cf2fb0ce13c18b420403",
+    "url": "/static/js/main.2186c7fe.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
